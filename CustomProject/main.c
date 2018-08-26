222901d3a71491437c8bcc709ae5e6f5f3d5e378
Christian Pomales <christianpomales@gmail.com>
2018-08-25 21:10:49 -0700

    Added sound

--- CustomProject/main.c
+++ CustomProject/main.c
@@ -9,8 +9,52 @@
 #include "io.h"
 #include "io.c"
 
-#define ON 1
-#define OFF 0
+#define PLAYERHITTERRAINSOUND 261.63
+#define PLAYERHITENEMYSOUND 293.66
+#define PROJECTILEFIREDSOUND 523.25
+#define PROJECTILEHITENEMYSOUND 440.00
+
+// audio functions
+void set_PWM(double frequency) {
+	// 0.954 hz is lowest frequency possible with this function,
+	// based on settings in PWM_on()
+	// Passing in 0 as the frequency will stop the speaker from generating sound
+	
+	static double current_frequency; // Keeps track of the currently set frequency
+	// Will only update the registers when the frequency changes, otherwise allows
+	// music to play uninterrupted.
+	if (frequency != current_frequency) {
+		if (!frequency) { TCCR3B &= 0x08; } //stops timer/counter
+		else { TCCR3B |= 0x03; } // resumes/continues timer/counter
+		
+		// prevents OCR3A from overflowing, using prescaler 64
+		// 0.954 is smallest frequency that will not result in overflow
+		if (frequency < 0.954) { OCR3A = 0xFFFF; }
+		
+		// prevents OCR0A from underflowing, using prescaler 64					// 31250 is largest frequency that will not result in underflow
+		else if (frequency > 31250) { OCR3A = 0x0000; }
+		
+		// set OCR3A based on desired frequency
+		else { OCR3A = (short)(8000000 / (128 * frequency)) - 1; }
+
+		TCNT0 = 0; // resets counter
+		current_frequency = frequency; // Updates the current frequency
+	}
+}
+
+void PWM_on() {
+	TCCR3A = (1 << COM0A0);
+	// COM3A0: Toggle PB3 on compare match between counter and OCR0A
+	TCCR3B = (1 << WGM02) | (1 << CS01) | (1 << CS00);
+	// WGM02: When counter (TCNT0) matches OCR0A, reset counter
+	// CS01 & CS30: Set a prescaler of 64
+	set_PWM(0);
+}
+
+void PWM_off() {
+	TCCR0A = 0x00;
+	TCCR3B = 0x00;
+}
 
 // keypad function
 unsigned char GetKeypadKey() {
@@ -565,6 +609,8 @@ int player_one_tick(int state) {
 enum player_one_projectile_tick_states {PLAYER_ONE_PROJECTILE_INIT, PLAYER_ONE_PROJECTILE_PLAYING};
 
 int player_one_projectile_tick(int state) {
+	set_PWM(0);
+	
 	switch(state) {
 		case PLAYER_ONE_PROJECTILE_INIT:
 			if (playFlag == 1)
@@ -600,6 +646,7 @@ int player_one_projectile_tick(int state) {
 		case PLAYER_ONE_PROJECTILE_PLAYING:
 			// create projectiles in array
 			if (key == '5') {
+				set_PWM(PROJECTILEFIREDSOUND);
 				for (unsigned char i = 0; i < 5; i++) {
 					if (player_one_projectile_location[i] == 255) {
 						player_one_projectile_location[i] = player_one_location;
@@ -788,6 +835,7 @@ int collision_tick(int state) {
 					for (unsigned char j = 0; j < 3; j++) {
 						if (enemy_location[j] != 255) {
 							if (player_one_projectile_location[i] == enemy_location[j]) {
+								set_PWM(PROJECTILEHITENEMYSOUND);
 								score++;
 								player_one_projectile_location[i] = 255;
 								enemy_location[j] = 255;
@@ -809,6 +857,7 @@ int collision_tick(int state) {
 					// make sure we only count lives lost once
 					if (terrainCollisionCounter < 2) {
 						if (lives > 1) {
+							set_PWM(PLAYERHITTERRAINSOUND);
 							lives--;
 						} else {
 						gameOverFlag = 1;
@@ -827,6 +876,7 @@ int collision_tick(int state) {
 					// make sure we only count lives lost once
 					if (enemyCollisionCounter < 2) {
 						if (lives > 1) {
+							set_PWM(PLAYERHITENEMYSOUND);
 							lives--;
 							} else {
 							gameOverFlag = 1;
@@ -946,6 +996,9 @@ int main() {
 
 	// initialize RNG
 	srand(time(NULL));
+	
+	// initialize PWM
+	PWM_on();
 
 	unsigned short i; // Scheduler for-loop iterator
 	while(1) {
